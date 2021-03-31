7524db9cec8f8829ffbecfbc1f6288f6b4b3c95a
angel contreras dueñas <c3po.angel@gmail.com>
2021-03-31 11:59:49 +0200

    new

--- app/src/main/java/Clouds.java
+++ app/src/main/java/Clouds.java
@@ -1,3 +1,5 @@
+import com.google.gson.annotations.SerializedName;
+
 public class Clouds{
 
 	@SerializedName("all")
--- app/src/main/java/Response.java
+++ app/src/main/java/Response.java
@@ -1,3 +1,7 @@
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class Response{
 
 	@SerializedName("visibility")
