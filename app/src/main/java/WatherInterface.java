bc2476391bb2857702c19ef4fd10461c8d12bbb4
angel contreras dueñas <c3po.angel@gmail.com>
2021-03-31 12:15:06 +0200

    interface

--- app/src/main/java/WatherInterface.java
+++
@@ -1,6 +0,0 @@
-import java.util.List;
-import retrofit2.Call;
-import retrofit2.http.GET;
-public interface WatherInterface {
-    @GET("");
-}
---
+++ app/src/main/java/com/angel/projectcapteurs/model/WatherInterface.java
@@ -0,0 +1,13 @@
+package com.angel.projectcapteurs.model;
+
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+//?lat=45.1834546&lon=5.742217&appid=2e2becf15ab311f47c7cd361617b06e4&units=metric&lang=fr
+/*
+http://api.openweathermap.org/data/2.5/weather?lat=45.1834546&lon=5.742217&appid=2e2becf15ab311f47c7cd361617b06e4&units=metric&lang=fr
+ */
+public interface WatherInterface {
+    @GET("/weather?lat={lat}&lon={lon}&appid=2e2becf15ab311f47c7cd361617b06e4&units=metric&lang=fr")
+    Call<List> getMeteo();
+}
