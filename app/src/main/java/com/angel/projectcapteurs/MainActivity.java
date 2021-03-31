1841daea4ad3ebe7021b329a11e7b8c4ef906000
esauMtz-MX <esaumtz4722@gmail.com>
2021-03-31 10:16:37 +0200

    init components

--- app/src/main/java/com/angel/projectcapteurs/MainActivity.java
+++ app/src/main/java/com/angel/projectcapteurs/MainActivity.java
@@ -3,12 +3,26 @@ package com.angel.projectcapteurs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtLatitude;
+    private TextView txtLongitude;
+    private ImageView imgBackGround;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initComponents();
+    }
+
+    private void initComponents() {
+        txtLatitude = findViewById(R.id.txtLatitude);
+        txtLongitude = findViewById(R.id.txtLongitude);
+        imgBackGround = findViewById(R.id.imgBackground);
     }
 }
